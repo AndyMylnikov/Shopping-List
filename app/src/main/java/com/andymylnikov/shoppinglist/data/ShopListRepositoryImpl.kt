0a489dad5899fb5b1abaa6bc8c70fb3428c565a7
Andy Mylnikov <andymylnikov@gmail.com>
2022-10-03 13:21:14 +0400

    Add LiveData

--- app/src/main/java/com/andymylnikov/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/andymylnikov/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,26 +1,32 @@
 package com.andymylnikov.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.andymylnikov.shoppinglist.domain.ShopItem
 import com.andymylnikov.shoppinglist.domain.ShopListRepository
 
-object ShopListRepositoryImpl: ShopListRepository {
+object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopList = mutableListOf<ShopItem>()
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private var autoIncrementId = 0
+
     init {
-        for (i in 0 until 10){
+        for (i in 0 until 10) {
             val item = ShopItem("Name $i", i, true)
             addShopItem(item)
         }
     }
 
     override fun addShopItem(shopItem: ShopItem) {
-        if(shopItem.id == ShopItem.UNDEFINED_ID) shopItem.id = autoIncrementId++
+        if (shopItem.id == ShopItem.UNDEFINED_ID) shopItem.id = autoIncrementId++
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -31,11 +37,14 @@ object ShopListRepositoryImpl: ShopListRepository {
 
     override fun getShopItem(shopItemId: Int): ShopItem {
         return shopList.find {
-            it.id ==shopItemId
-        }?: throw  RuntimeException("Element with id $shopItemId not found")
+            it.id == shopItemId
+        } ?: throw  RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/andymylnikov/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/andymylnikov/shoppinglist/presentation/MainActivity.kt
@@ -8,7 +8,8 @@ import com.andymylnikov.shoppinglist.R
 import com.andymylnikov.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
 
     private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +17,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.shopList.observe(this){
+        viewModel.shopList.observe(this) {
             Log.d("MainActivityTest", it.toString())
+            if (count == 0){
+                count++
+            val item = it[0]
+            viewModel.deleteShopItem(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
