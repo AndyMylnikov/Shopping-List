1662f5f6a2d2fce52973445e9d2e5a146d4f3d7f
Andy Mylnikov <andymylnikov@gmail.com>
2022-10-03 09:44:40 +0400

    Add data object. Data layer implementation.

--- app/src/main/java/com/andymylnikov/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/andymylnikov/shoppinglist/domain/ShopListRepository.kt
@@ -5,7 +5,7 @@ interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
     fun deleteShopItem(shopItem: ShopItem)
     fun editShopItem(shopItem: ShopItem)
-    fun getShopItem(shopItem: Int):ShopItem
+    fun getShopItem(shopItemId: Int):ShopItem
     fun getShopList(): List<ShopItem>
 
 }
